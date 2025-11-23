450bcc9aac02f6039ca34101397794290c5ac340
r.ciesielski3 <r.ciesielski3@gmail.com>
2025-11-23 13:57:42 +0100

    faet: add example script

---
+++ exampleScript.js
@@ -0,0 +1,16 @@
+import { randomSleep } from "../../common/utils.js";
+import { Crocodile } from "../../objects/crocodile.js";
+import { randomFromArray } from "../../common/utils.js";
+
+export function exampleOne(data) {
+  const crocodile = new Crocodile(__ENV.URL);
+  const payload = data.example;
+
+  crocodile.postLogin(payload);
+  crocodile.getList();
+  const ids = crocodile.getResponse().json("#.id");
+  const id = randomFromArray(ids);
+  crocodile.getItem(id);
+
+  randomSleep(1000, 2000);
+}
